29036a29962624900828f0b5c9f6fa07340ce605
anvarbekov <mustafoanvarbekov24@gmail.com>
2024-05-20 19:39:16 +0500

    https

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 
-axios.defaults.baseURL = "http://zoyirbek.pro/api/";
+axios.defaults.baseURL = "https://zoyirbek.pro/api/";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
